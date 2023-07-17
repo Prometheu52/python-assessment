6166c27dd658a869f04d535ada56087fedbb0fb9
Geri <mg.tigris1@gmail.com>
2023-07-17 14:11:39 +0200

    Intermission commit

--- main.py
+++ main.py
@@ -3,35 +3,47 @@ import sys
 import logging as log
 import os
 
-def main(path: str) -> None:
+from pptx import Presentation
+
+def main(path: str):
     if not os.path.exists(path):
         log.critical("The provided path does not exists")
-        exit()
+        exit_on_failure()
 
     try:
-        file = open(path)
+        json_file = open(path)
+        config: object = json.load(json_file)
     except OSError as err:
         log.critical(f"Could not open file with error: {err}")
-        exit()
-    config = json.load(file)
+        exit_on_failure()
+    except ValueError as err:
+        log.critical(f"The provided file is not in a valid json format, error: {err}")
+        exit_on_failure()
 
-    for i in config["presentation"]:
+    # Presentation raises ValueError if the file is not a ppt file
+    prs = Presentation()
+
+    for i in prs.slide_layouts:
         print(i)
 
+def exit_on_failure():
+    print("Something went wrong, check the log.txt for further info!")
+    exit()
 
 if __name__ == "__main__":
+    # TODO: Check if the python-pptx modul is installed
     # Init logging
     log.basicConfig(filename="log.txt", encoding='utf-8', level=log.INFO, format='%(asctime)s |%(levelname)s| %(message)s')
     # only one commandline arg: json config
     match len(sys.argv):
         case 1:
             log.critical("Insufficient number of arguments")
-            exit()
+            exit_on_failure()
         case 2:
             log.info("Starting execution")
-            print(f"argv: {sys.argv[1]}")
+            # print(f"argv: {sys.argv[1]}")
             main(sys.argv[1])
         case other:
             log.critical(f"More arguments than needed (provided: { len(sys.argv)-1 })")
-            exit()
+            exit_on_failure()
     log.info("Finished execution")
